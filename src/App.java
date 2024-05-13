f6e833dde621b275f9d3b1bb99d9309206cff57a
0xguidev <guilhermeviniciustrabalho@gmail.com>
2024-05-13 14:14:06 -0300

    Start the resolution of exercise 009 - account balance

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter account number: ");
+        System.out.println("Enter account holder: ");
+        System.out.println("Is there an initial deposit (y/n)? ");
+        System.out.println("Enter initial deposit value: ");
+
+        System.out.println("Account Data: ");
+
+
+        scanner.close();
     }
 }
